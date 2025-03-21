145495339ca4535000fc0ec0d6c3b9bc72532aab
Ahmed Haitham <haithamelsersy5060@gmail.com>
2025-03-21 17:06:48 +0200

    first commit

---
+++ src/test/java/com/example/late_plate_backend/LatePlateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.late_plate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatePlateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
